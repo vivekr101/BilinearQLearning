9f35bbc5f0193a1f5edfdde720bd0a1ef26dc906
Vivek <vivekr101@gmail.com>
2012-02-06 18:38:43 -0800

    Cleaned up getReward in TemplateDir, init-ed Acrobat problem.

---
+++ +Acrobat/getExploreAction.m
@@ -0,0 +1,11 @@
+function action = getExploreAction(state)
+%{
+Function that is used for getting exploratory actions, used to create
+training samples.
+
+Args:
+    state: state from which to take action, row vector
+
+Returns:
+    action: action to be taken, row vector.
+%}
